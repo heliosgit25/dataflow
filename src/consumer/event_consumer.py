bd469ab928af9700effe3c7479e98862ea341123
heliosgit <twishampatighosh1@gmail.com>
2026-05-12 03:30:20 +0530

    Created connection to postgre db, using psycopg2. Also edited the for loop to write data to the user_events table using cursor.execute. Also fixed datetime format and other issues.

--- src/consumer/event_consumer.py
+++ src/consumer/event_consumer.py
@@ -1,5 +1,7 @@
 from kafka import KafkaConsumer
 import json
+import psycopg2
+from datetime import datetime
 
 consumer = KafkaConsumer(
     "ecommerce-events",
@@ -10,8 +12,30 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode("utf-8"))
 )
 
+# DB Connection
+conn = psycopg2.connect(
+    dbname="ecommerce",
+    user="helios",
+    password="",
+    host="localhost",
+    port="5432"
+)
+cursor = conn.cursor()
+
 print("Consumer started...")
 
 for message in consumer:
     event = message.value
     print("Received:", event)
+    event_time = datetime.fromisoformat(event["timestamp"])
+    cursor.execute("""
+    INSERT INTO user_events (user_id, product, price, event_time)
+    VALUES (%s, %s, %s, %s)
+    """, (
+    event["user_id"],
+    event["product"],
+    event["price"],
+    event_time
+    ))
+
+    conn.commit()
